0babc5653964cbe00939bfabaeabfb62319aa2bf
Niraimadi <66937110+Niraimadi@users.noreply.github.com>
2021-06-26 19:09:26 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic web development react js bootcamp</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and Node.js</h1>
+        <p>A basic web development node js bootcamp</p>
+      </div>
+      <div className="note">
+        <h1>Tableau Fundamentals</h1>
+        <p>A basic bootcamp on tableau fundamentals</p>
+      </div>
+      <div className="note">
+        <h1>Data Structures</h1>
+        <p>A basic bootcamp on Data structures Java</p>
+      </div>
+      <div className="note">
+        <h1>Mobile App development</h1>
+        <p>A basic bootcamp on developing mobile apps</p>
+      </div>
+      <div className="note">
+        <h1>OS fundamentals</h1>
+        <p>A basic bootcamp on fundamentals of OS</p>
+      </div>
+    </div>
+  );
+}
+export default Info;
